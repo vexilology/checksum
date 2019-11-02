2b9c55a506af0c692d0d9ed448c1d00344192a04
vexilology <andreyisback@yandex.ru>
2019-11-02 21:07:43 +0400

    micro fix

--- binary.cc
+++ binary.cc
@@ -15,76 +15,75 @@ int main()
 	cout << "Enter second binary string\n";
 	cin >> b;
 
-	if(strlen(a)==strlen(b))
+	if(strlen(a) == strlen(b))
 	{
-		char carry='0';
-		int length=strlen(a);
+		char carry = '0';
+		int length = strlen(a);
 
-		for(i=length-1;i>=0;i--)
+		for(i=length-1 ;i>=0; i--)
 		{
 			if(a[i]=='0' && b[i]=='0' && carry=='0')
 			{
-				sum[i]='0';
-				carry='0';
+				sum[i] = '0';
+				carry = '0';
 			}
 			else if(a[i]=='0' && b[i]=='0' && carry=='1')
 			{
-				sum[i]='1';
-				carry='0';
+				sum[i] = '1';
+				carry = '0';
 			}
 			else if(a[i]=='0' && b[i]=='1' && carry=='0')
 			{
-				sum[i]='1';
-				carry='0';
+				sum[i] = '1';
+				carry = '0';
 			}
 			else if(a[i]=='0' && b[i]=='1' && carry=='1')
 			{
-				sum[i]='0';
-				carry='1';
+				sum[i] = '0';
+				carry = '1';
 			}
 			else if(a[i]=='1' && b[i]=='0' && carry=='0')
 			{
-				sum[i]='1';
-				carry='0';
+				sum[i] = '1';
+				carry = '0';
 			}
 			else if(a[i]=='1' && b[i]=='0' && carry=='1')
 			{
-				sum[i]='0';
-				carry='1';
+				sum[i] = '0';
+				carry = '1';
 			}
 			else if(a[i]=='1' && b[i]=='1' && carry=='0')
 			{
-				sum[i]='0';
-				carry='1';
+				sum[i] = '0';
+				carry = '1';
 			}
 			else if(a[i]=='1' && b[i]=='1' && carry=='1')
 			{
-				sum[i]='1';
-				carry='1';
+				sum[i] = '1';
+				carry = '1';
 			}
 			else
 				break;
 		}
 		cout << "\nSum = " << carry << sum;
 
-		for(i=0;i<length;i++)
+		for(i=0; i<length; i++)
 		{
 			if(sum[i]=='0')
-				complement[i]='1';
+				complement[i] = '1';
 			else
-				complement[i]='0';
+				complement[i] = '0';
 		}
 
 		if(carry=='1')
-			carry='0';
+			carry = '0';
 		else
-			carry='1';
+			carry = '1';
 
 		cout << "\nChecksum = " << carry << complement;
 	}
 	else
 		cout << "Wrong input strings\n";
 
-    system("pause");
-	return 0;
+    system("pause >> NUL");
 }
