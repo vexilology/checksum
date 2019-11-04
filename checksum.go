d68998c3a3a0a90b3d2589fbb79cc3453cde4249
vexilology <andreyisback@yandex.ru>
2019-11-05 03:03:43 +0400

    add go check

--- checksum.go
+++ checksum.go
@@ -10,12 +10,12 @@ import (
 
 func main() {
 
-	password := "test sum"
+	privatemessage := "test sum"
 
-	MD5 := fmt.Sprintf("%x", md5.Sum([]byte(password)))
-	SHA1 := fmt.Sprintf("%x", sha1.Sum([]byte(password)))
-	SHA256 := fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
-	SHA512 := fmt.Sprintf("%x", sha512.Sum512([]byte(password)))
+	MD5 := fmt.Sprintf("%x", md5.Sum([]byte(privatemessage)))
+	SHA1 := fmt.Sprintf("%x", sha1.Sum([]byte(privatemessage)))
+	SHA256 := fmt.Sprintf("%x", sha256.Sum256([]byte(privatemessage)))
+	SHA512 := fmt.Sprintf("%x", sha512.Sum512([]byte(privatemessage)))
 
 	fmt.Println("MD5: ", MD5)
 	fmt.Println("SHA-1: ", SHA1)
