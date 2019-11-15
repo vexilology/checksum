eee09da07fc28380fda8a7473a20e64b9e93d0db
vexilology <andreyisback@yandex.ru>
2019-11-15 04:11:33 +0400

    check mess go

--- checksummess.go
+++ checksummess.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"strconv"
 	"fmt"
+	"hash/crc32"
+	"hash/adler32"
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
@@ -10,18 +13,26 @@ import (
 
 func main() {
 
-	var privatemessage string
+	var data string
 
 	fmt.Println("Enter the message:")
-	fmt.Scan(&privatemessage)
+	fmt.Scan(&data)
 
-	MD5 := fmt.Sprintf("%x", md5.Sum([]byte(privatemessage)))
-	SHA1 := fmt.Sprintf("%x", sha1.Sum([]byte(privatemessage)))
-	SHA256 := fmt.Sprintf("%x", sha256.Sum256([]byte(privatemessage)))
-	SHA512 := fmt.Sprintf("%x", sha512.Sum512([]byte(privatemessage)))
+	firstCRC32 := crc32.ChecksumIEEE([]byte(data))
+	secondCRC32 := strconv.FormatUint(uint64(firstCRC32), 16)
+	firstADLER32 := adler32.Checksum([]byte(data))
+	secondADLER32 := strconv.FormatUint(uint64(firstADLER32), 16)
+	MD5 := fmt.Sprintf("%x", md5.Sum([]byte(data)))
+	SHA1 := fmt.Sprintf("%x", sha1.Sum([]byte(data)))
+	SHA256 := fmt.Sprintf("%x", sha256.Sum256([]byte(data)))
+	SHA384 := fmt.Sprintf("%x", sha512.Sum384([]byte(data)))
+	SHA512 := fmt.Sprintf("%x", sha512.Sum512([]byte(data)))
 
-	fmt.Println("MD5: ", MD5)
-	fmt.Println("SHA-1: ", SHA1)
-	fmt.Println("SHA-256: ", SHA256)
-	fmt.Println("SHA-512: ", SHA512)
+	fmt.Println("CRC32:", secondCRC32)
+	fmt.Println("ADLER32:", secondADLER32)
+	fmt.Println("MD5:", MD5)
+	fmt.Println("SHA1:", SHA1)
+	fmt.Println("SHA256:", SHA256)
+	fmt.Println("SHA384:", SHA384)
+	fmt.Println("SHA512:", SHA512)
 }
