cb5b759b091863f9ea58072c626bc4c6631afbba
vexilology <andreyisback@yandex.ru>
2019-11-18 21:33:31 +0400

    file go

--- checksumfile.go
+++ checksumfile.go
@@ -31,6 +31,7 @@ func main() {
 	firstadler32 := adler32.Checksum([]byte(data))
 	secondadler32 := strconv.FormatUint(uint64(firstadler32), 16)
 
+	fmt.Print("-----------------------\n")
 	fmt.Println("CRC32:", secondcrc32)
 	fmt.Println("ADLER32:", secondadler32)
 	fmt.Printf("MD5: %x\n", md5.Sum(data))
@@ -38,4 +39,5 @@ func main() {
 	fmt.Printf("SHA256: %x\n", sha256.Sum256(data))
 	fmt.Printf("SHA384: %x\n", sha512.Sum384(data))
 	fmt.Printf("SHA512: %x\n", sha512.Sum512(data))
+	fmt.Print("-----------------------")
 }
