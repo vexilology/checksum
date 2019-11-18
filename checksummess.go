66fa0854f69aba348ed17820c235c84ead2d39fa
vexilology <andreyisback@yandex.ru>
2019-11-18 23:50:35 +0400

    update lines

--- checksummess.go
+++ checksummess.go
@@ -17,7 +17,7 @@ func main() {
 
 	var data string
 
-	fmt.Println("Enter your message: ")
+	fmt.Println("**** Enter your message ****")
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
 		data = scanner.Text()
@@ -27,19 +27,24 @@ func main() {
 	secondCRC32 := strconv.FormatUint(uint64(firstCRC32), 16)
 	firstADLER32 := adler32.Checksum([]byte(data))
 	secondADLER32 := strconv.FormatUint(uint64(firstADLER32), 16)
-	MD5 := fmt.Sprintf("% x", md5.Sum([]byte(data)))
-	SHA1 := fmt.Sprintf("% x", sha1.Sum([]byte(data)))
-	SHA256 := fmt.Sprintf("% x", sha256.Sum256([]byte(data)))
-	SHA384 := fmt.Sprintf("% x", sha512.Sum384([]byte(data)))
-	SHA512 := fmt.Sprintf("% x", sha512.Sum512([]byte(data)))
+	MD5 := fmt.Sprintf("%x", md5.Sum([]byte(data)))
+	SHA1 := fmt.Sprintf("%x", sha1.Sum([]byte(data)))
+	SHA256 := fmt.Sprintf("%x", sha256.Sum256([]byte(data)))
+	SHA384 := fmt.Sprintf("%x", sha512.Sum384([]byte(data)))
+	SHA512 := fmt.Sprintf("%x", sha512.Sum512([]byte(data)))
 
 	fmt.Print("-----------------------\n")
 	fmt.Println("CRC32:", secondCRC32)
+	fmt.Println("===")
 	fmt.Println("ADLER32:", secondADLER32)
+	fmt.Println("===")
 	fmt.Println("MD5:", MD5)
+	fmt.Println("===")
 	fmt.Println("SHA1:", SHA1)
+	fmt.Println("===")
 	fmt.Println("SHA256:", SHA256)
+	fmt.Println("===")
 	fmt.Println("SHA384:", SHA384)
+	fmt.Println("===")
 	fmt.Println("SHA512:", SHA512)
-	fmt.Print("-----------------------")
 }
